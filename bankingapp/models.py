853c74f0eee169331a39c73575f1b3c19cd6fe3e
Riya <riya@Riyas-MacBook-Air.local>
2022-06-16 16:38:52 +0530

    models added

--- bankingapp/models.py
+++ bankingapp/models.py
@@ -1,8 +1,42 @@
 from django.db import models
 
 # Create your models here.
-class Bank(models.Model):
-    name=models.CharField(max_length=200)
-      
-    def __str__(self) :
-        return self.name
+from pyexpat import model
+from django.db import models
+
+# Create your models here.
+
+
+class groups(models.Model):
+    group=models.CharField(max_length=225)
+
+
+    def _str_(self):
+     return self.group
+
+class ledger(models.Model):
+    group=models.ForeignKey(groups,on_delete=models.CASCADE,blank=False)
+    name=models.CharField(max_length=225)
+    
+    def _str_(self):
+     return self.name
+
+
+class contra(models.Model):
+    account=models.ForeignKey(ledger,on_delete=models.CASCADE,blank=False)
+    Particulars=models.ForeignKey(ledger,on_delete=models.CASCADE,blank=False)
+    amount=models.IntegerField()
+    transactiontype=models.ForeignKey(ledger,on_delete=models.CASCADE,blank=False)
+
+
+        
+    def _str_(self):
+     return self.account
+
+class receipt(models.Model):
+    account=models.ForeignKey(ledger,on_delete=models.CASCADE,blank=False)
+    Particulars=models.ForeignKey(ledger,on_delete=models.CASCADE,blank=False)
+    amount=models.IntegerField()
+    transactiontype=models.ForeignKey(ledger,on_delete=models.CASCADE,blank=False)
+     
+    
